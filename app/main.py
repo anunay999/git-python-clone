d2a3628925722e8b341b564b265106555ec20459
anunay.aatipamula <anunay.aatipamula@salesforce.com>
2024-05-12 14:42:15 +0530

    Read Blob

--- app/main.py
+++ app/main.py
@@ -1,23 +1,11 @@
 import sys
 import os
+import zlib
 
 
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
-    print("Logs from your program will appear here!")
-
-    # Uncomment this block to pass the first stage
-    #
-    # command = sys.argv[1]
-    # if command == "init":
-    #     os.mkdir(".git")
-    #     os.mkdir(".git/objects")
-    #     os.mkdir(".git/refs")
-    #     with open(".git/HEAD", "w") as f:
-    #         f.write("ref: refs/heads/main\n")
-    #     print("Initialized git directory")
-    # else:
-    #     raise RuntimeError(f"Unknown command #{command}")
+    
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -26,6 +14,14 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Intialized git directory")
+    elif command == "cat-file":
+        param, hash = sys.argv[2], sys.argv[3]
+        if param == '-p':
+            with open(".git/objects/{hash[:2]}/{hash[2:]}", "rb") as f:
+                compressed = f.read()
+                content = zlib.decompress(compressed).split(b'\0')[1].decode('utf-8')
+                print(content, end="")
+
     else:
         raise RuntimeError(f"Unknown command #{command}")
     
