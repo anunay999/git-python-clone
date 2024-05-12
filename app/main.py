2ea5dba4ad27b7b4d278ba1fa0dc0e7adecfbdab
anunay.aatipamula <anunay.aatipamula@salesforce.com>
2024-05-12 18:55:30 +0530

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -33,6 +33,18 @@ def main():
                 with open(f".git/objects/{hash[:2]}/{hash[2:]}", "wb") as wb:
                     wb.write(zlib.compress(content))
                 print(hash)
+    elif command == "ls-tree":
+        param, hash = sys.argv[2], sys.argv[3]
+        if(param == '--name-only'):
+            with open(f".git/objects/{hash[:2]}/{hash[2:]}", "rb") as f:
+                data = zlib.decompress(f.read())
+                _, binary_data = data.split(b'\x00', maxsplit=1)
+                while binary_data:
+                    mode, binary_data = binary_data.split(b'\x00', maxsplit=1)
+                    _, name = mode.split()
+                    binary_data = binary_data[20:]
+                    print(name.decode('utf-8'))
+
 
     else:
         raise RuntimeError(f"Unknown command #{command}")
