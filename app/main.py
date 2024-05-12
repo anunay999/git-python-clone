a88009a19f7cfdcdc722779b580ce19300ab062f
anunay.aatipamula <anunay.aatipamula@salesforce.com>
2024-05-12 20:07:54 +0530

    Write a tree

--- app/main.py
+++ app/main.py
@@ -3,9 +3,49 @@ import os
 import zlib
 import hashlib
 
+DIR_MODE = 40000
+FILE_MODE = 100644
+
+def hash_blob(content, type = 'blob') -> str:
+    header = f'{type} {len(content)}\0'.encode()
+    content = header + content
+    hash = hashlib.sha1(content).hexdigest()
+    os.makedirs(f".git/objects/{hash[:2]}",exist_ok=True)
+    with open(f".git/objects/{hash[:2]}/{hash[2:]}", "wb") as wb:
+        wb.write(zlib.compress(content))
+    return hash
+
+def write_blob(file_path: str) -> str:
+    with open(f"{file_path}", "rb") as f:
+        return hash_blob(f.read())
+        
+def encode_mode(mode, name, hash) -> bytes:
+    return f"{mode} {name}".encode() + b'\0' + bytes.fromhex(hash)
+
+def write_tree(root = ".") -> str:
+    hash_map = {}
+    for entry in os.scandir(root):
+        if entry.name.startswith(('.', '_')):
+            continue
+        if entry.is_file():
+            hash = write_blob(os.path.join(root, entry.name))
+            hash_map[entry.name] = encode_mode(FILE_MODE, entry.name, hash)
+        else:
+            hash = write_tree(os.path.join(root, entry.name))
+            hash_map[entry.name] = encode_mode(DIR_MODE, entry.name, hash)
+    
+    hash_list = [v for k, v in sorted(hash_map.items())]
+
+    content = b''.join(hash_list)
+
+    return hash_blob(content, 'tree')
+
+
 def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     
+    # cwd = os.getcwd()
+
     command = sys.argv[1]
     if command == "init":
         os.mkdir(".git")
@@ -14,6 +54,7 @@ def main():
         with open(".git/HEAD", "w") as f:
             f.write("ref: refs/heads/main\n")
         print("Intialized git directory")
+
     elif command == "cat-file":
         param, hash = sys.argv[2], sys.argv[3]
         if param == '-p':
@@ -21,18 +62,12 @@ def main():
                 compressed = f.read()
                 content = zlib.decompress(compressed).split(b'\0')[1].decode('utf-8')
                 print(content, end="")
+
     elif command == "hash-object":
         param, file = sys.argv[2], sys.argv[3]
         if param == '-w':
-            with open(f"{file}", "rb") as f:
-                content = f.read()
-                header = f'blob {len(content)}\0'.encode()
-                content = header + content
-                hash = hashlib.sha1(content).hexdigest()
-                os.makedirs(f".git/objects/{hash[:2]}",exist_ok=True)
-                with open(f".git/objects/{hash[:2]}/{hash[2:]}", "wb") as wb:
-                    wb.write(zlib.compress(content))
-                print(hash)
+            print(write_blob(file))
+
     elif command == "ls-tree":
         param, hash = sys.argv[2], sys.argv[3]
         if(param == '--name-only'):
@@ -43,9 +78,10 @@ def main():
                     mode, binary_data = binary_data.split(b'\x00', maxsplit=1)
                     _, name = mode.split()
                     binary_data = binary_data[20:]
-                    print(name.decode('utf-8')) 
+                    print(name.decode('utf-8'))
 
-
+    elif command == 'write-tree':
+        print(write_tree())
     else:
         raise RuntimeError(f"Unknown command #{command}")
     
